3f480e1cf12e90f8bf4fb194e38579ebca10dc53
Darwin224 <diromero@unah.hn>
2025-03-12 07:23:08 -0600

    creando proyecto

---
+++ src/main/java/hn/unah/ing/swappingbackend/SwappingbackendApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.ing.swappingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwappingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwappingbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/unah/ing/swappingbackend/SwappingbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.ing.swappingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwappingbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
